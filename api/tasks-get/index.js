c0d010dc5f14379de675af309200027bf17d2777
Matthias Tartofreze <matthias.flament@ynov.com>
2021-10-04 16:34:23 +0200

    api fakeDb

--- api/tasks-get/index.js
+++ api/tasks-get/index.js
@@ -18,9 +18,12 @@ module.exports = async function (context, req) {
             status: ""
         }
     ];
+    const responseMessage = tasks
+        ? "Hello, " + tasks + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: tasks
+        body: responseMessage
     };
 }
